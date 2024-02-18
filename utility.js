03ad627900ca06b6fd59ac3bbc0041b00dd8a98e
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-02-18 22:19:44 +0600

    added discount part

--- utility.js
+++ utility.js
@@ -1,91 +1,107 @@
-
 const allSeatButtons = document.getElementsByClassName("kbd");
 // console.log(allSeatButton)
 
+const coupon15 = "NEW15";
+const coupon20 = "Couple 20";
+
 let count = 0;
 let currentAvalibaleSeat = 40;
-let coupon15 = 'NEW15'
-let coupon20 ='Couple 20'
+
 for (const allSeatButton of allSeatButtons) {
-    // console.log(allSeatButton)
-    allSeatButton.addEventListener('click', function (e) {
-        // console.log('inshallah')
-        e.target.style.backgroundColor = "green";
-        count = count + 1;
+  // console.log(allSeatButton)
+  allSeatButton.addEventListener("click", function (e) {
+    // console.log('inshallah')
+    e.target.style.backgroundColor = "green";
+    count = count + 1;
 
-        document.getElementById('purches-seat-count').innerText = count;
+    document.getElementById("purches-seat-count").innerText = count;
 
-        document.getElementById('current-available-seat').innerText = currentAvalibaleSeat - count;
+    document.getElementById("current-available-seat").innerText =
+      currentAvalibaleSeat - count;
 
-const classCatagory = 'Economoy'
-        const seat = e.target.innerText;
+    const classCatagory = "Economoy";
+    const seat = e.target.innerText;
 
-        const vara = document.getElementById("vara-amount").innerText;
-        const varaConvrt = parseInt(vara)
+    const vara = document.getElementById("vara-amount").innerText;
+    const varaConvrt = parseInt(vara);
 
-// console.log(varaConvrt)
-        //         console.log(seat)
-        
-        const showPurchesSeatInfo = document.getElementById('show-purches-seat-info');
+    // console.log(varaConvrt)
+    //         console.log(seat)
 
-        const li = document.createElement("li")
+    const showPurchesSeatInfo = document.getElementById(
+      "show-purches-seat-info"
+    );
 
-        const p = document.createElement("p")
-        p.innerText = seat;
+    const li = document.createElement("li");
 
-        const p1 = document.createElement("p")
-        p1.innerText = classCatagory;
-        const p2=document.createElement("p")
-        p2.innerText = varaConvrt;
+    const p = document.createElement("p");
+    p.innerText = seat;
 
-        li.appendChild(p)
-        li.appendChild(p1)
-        li.appendChild(p2)
-        showPurchesSeatInfo.appendChild(li)
+    const p1 = document.createElement("p");
+    p1.innerText = classCatagory;
+    const p2 = document.createElement("p");
+    p2.innerText = varaConvrt;
+
+    li.appendChild(p);
+    li.appendChild(p1);
+    li.appendChild(p2);
+    showPurchesSeatInfo.appendChild(li);
 
     //   total cost section
-        const totalCost = document.getElementById('total-price').innerText;
-        const convertTotalCost=parseInt(totalCost)
-        document.getElementById('total-price').innerText = convertTotalCost + varaConvrt;
+    const totalCost = document.getElementById("total-price").innerText;
+    const convertTotalCost = parseInt(totalCost);
+    document.getElementById("total-price").innerText =
+      convertTotalCost + varaConvrt;
 
-        
-        // grand total section
-        const grandTotal = document.getElementById('grand-total').innerText;
-        const convartGrandTotal = parseInt(grandTotal);
-        document.getElementById('grand-total').innerText = convartGrandTotal + varaConvrt;
-
-    })
-
-    
+    // grand total section
+    const grandTotal = document.getElementById("grand-total").innerText;
+    const convartGrandTotal = parseInt(grandTotal);
+    document.getElementById("grand-total").innerText =
+      convartGrandTotal + varaConvrt;
+  });
 }
 
 function play() {
-    const homeScreen = document.getElementById('home-screen');
-   homeScreen.classList.add('hidden')
-    const popup = document.getElementById('popup');
-    popup.classList.remove('hidden')
-
+  const homeScreen = document.getElementById("home-screen");
+  homeScreen.classList.add("hidden");
+  const popup = document.getElementById("popup");
+  popup.classList.remove("hidden");
 }
 
-const applyBtn = document.getElementById('apply-btn');
-// console.log(applyBtn)
 
-// applyBtn.addEventListener("click", function () {
-//     // console.log('alhamdulliah')
+// bonus part section..........................................
 
-//     const inputCoupon = document.getElementById('input-coupon');
-//     // console.log(inputCoupon.value)
+const vara = document.getElementById("vara-amount").innerText;
+const varaConvrt = parseInt(vara);
+
+const applyBtn = document.getElementById("apply-btn");
+
+applyBtn.addEventListener("click", function () {
+  console.log('alhamdulliah')
+
+    const inputCoupon = document.getElementById("input-coupon").value;
+    console.log(inputCoupon)
+    console.log(inputCoupon)
+        console.log(coupon15)
+    if (coupon15 === inputCoupon) {
+        console.log('coupon15')
+        
+        const finalElement = document.getElementById('grand-total')
+        const discount = varaConvrt * 0.15;
+finalElement.innerText=discount
+
+        // const finalElement1 = document.getElementById('grand-total')
+        // const discount1 = varaConvrt * 0.15;
+        // finalElement1.value=discount1
+        
+
+    } else if (coupon20 === inputCoupon) {
+        console.log('coupon20')
+    } else {
+        alert('error')
+    }
+  
     
-//     if ( coupon15 === 'NEW15'){
-   
-//         const discout = varaConvrt * 0.15
-//         const finalDiscountValue = document.getElementById('grand-total');
-//         finalDiscountValue.innerText = varaConvrt - discout;
-
-
-//     } else if (coupon20 === 'Couple 20'){
-//         console.log('25')
-// }else {
-//     alert ('vul hoyce vai')
-//     }
-// })
+    
+}
+);
